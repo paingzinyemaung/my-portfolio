869ea111756f0a2ac31878165cab24b5d9cff414
PaingZinYeMaung <paingzinyemaung.pgo@gmail.com>
2026-06-18 00:38:24 +0630

    Initial commit: React + Tailwind setup

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <div className="flex justify-center items-center h-screen bg-slate-900">
+      <h1 className="text-4xl font-bold text-sky-400 drop-shadow-md">
+        Tailwind works perfectly!
+      </h1>
+    </div>
+  );
+};
+
+export default App;
